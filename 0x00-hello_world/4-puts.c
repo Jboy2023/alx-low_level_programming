c4db23d6aeb0649305ac55db53e9d816932752b6
jboy2023 <costrafred33@gmail.com>
2023-06-16 07:11:02 -0700

    created the 4-put.c file which prints "Programming is like building a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0; sucess
+ */
+
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(Mystr);
+
+	return (0);
+}
