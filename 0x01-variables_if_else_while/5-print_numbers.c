a78ca0c9fe4979332b2857137674cbe53c77a715
jboy2023 <costrafred33@gmail.com>
2023-06-18 11:57:12 -0700

    created the 5-print_numbers.c program which print the numbers of base 10 starting from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main - entry point
+ * Description: prints all single digit numbers of base 10 starting from 0
+ * Return: 0; success
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar (x);
+	}
+	putchar ('\n');
+
+	return (0);
+}
