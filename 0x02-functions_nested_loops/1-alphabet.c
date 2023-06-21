1bcd716c7bbb08dab53f5a369bbfb681280121a7
jboy2023 <costrafred33@gmail.com>
2023-06-21 08:02:55 -0700

    craeted the 1-alphabet.c program which prints the alphabet a-z in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: prints the alphabet, in lowercase, followed by a new line
+ * You can only use _putchar twice in your code
+ * Return: 0; success
+ */
+
+void print_alphabet(void)
+
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
