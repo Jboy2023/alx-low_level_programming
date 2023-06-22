2dc268ef39fec622bf4f25d95f6d707b7efc9bc3
jboy2023 <costrafred33@gmail.com>
2023-06-22 10:23:59 -0700

    updated the 0-main.c and main.h

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * positive_or_negative - entery point
+ * @n: integer
+ * Return: void
+ */
+void positive_or_negative(int n)
+{
+	if (n > 0)
+	{
+		/* if the condition is true then print the following */
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		/* else if the condition is true then print the following */
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		/* if none of the conditions is true */
+		printf("%d is negative\n", n);
+	}
+}
