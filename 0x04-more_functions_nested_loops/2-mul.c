5a22e16886976e7d4236b9c540b6472ae5cdee10
jboy2023 <costrafred33@gmail.com>
2023-06-23 14:49:15 -0700

    created the 2-mul.c program

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - entery point
+ * Description:a function that multiplies two integers.
+ * @a: integer
+ * @b: integer
+ * Return: void
+ */
+
+int mul(int a, int b)
+
+{
+	int x;
+
+	x = a * b;
+	return (x);
+	_putchar('\n');
+}
