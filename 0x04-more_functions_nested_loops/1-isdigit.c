140bd6dec9662c1c78d6df508a5314943677219f
jboy2023 <costrafred33@gmail.com>
2023-06-23 14:31:30 -0700

    created the 1-isdigit.c program

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - entery point
+ * Description:a function that checks for a digit (0 through 9)
+ * @c: integer
+ * Returns 1 if c is a digit
+ * Returns 0 otherwise
+ * Return: 1 || 0
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
