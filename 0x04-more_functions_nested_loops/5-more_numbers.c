c6b8369ca741e4d77bb7750c8b5c002ff2f00169
jboy2023 <costrafred33@gmail.com>
2023-06-24 08:38:58 -0700

    created the 5-more_number.c program

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - entry point
+ * Description: a function that prints 10 times the numbers, from 0 to 14
+ *  followed by a new line
+ *  You can only use _putchar three times in your code
+ *  Return: void
+ */
+
+void more_numbers(void)
+
+{
+	int x;
+	int y;
+
+	for (x = '0'; x < "10"; x++)
+	{
+		for (y = '0'; y <= "14"; y++)
+		{
+			_putchar(y);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
