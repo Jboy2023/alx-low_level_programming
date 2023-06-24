9a029228045524445ea0409e0fdf104573ffece7
jboy2023 <costrafred33@gmail.com>
2023-06-24 10:14:27 -0700

    updated the 5-more_number.c program

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -19,10 +19,10 @@ void more_numbers(void)
 		{
 			if (y > 9)
 			{
-				_putchar((y / 10) + '0');
+				_putchar(y / 10 + '0');
 			}
-			_putchar((y % 10) + '0');
+			_putchar(y % 10 + '0');
 		}
-		_putchat('\n');
+		_putchar('\n');
 	}
 }
