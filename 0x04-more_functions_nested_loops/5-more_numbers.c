e56f75ac078a03140f6b775b037a37a69bf24ac8
jboy2023 <costrafred33@gmail.com>
2023-06-24 08:46:55 -0700

    updated the 5-more_number.c program

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,13 @@ void more_numbers(void)
 
 {
 	int x;
-	char y;
+	int y;
 
 	for (x = '0'; x < "10"; x++)
 	{
 		for (y = '0'; y <= "14"; y++)
 		{
-			_putchar(y);
+			_putchar("%d",y);
 		}
 		_putchar('\n');
 	}
