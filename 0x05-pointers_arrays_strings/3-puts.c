93903a7a63b34952c090fbec54e22c44d1256156
jboy2023 <costrafred33@gmail.com>
2023-06-27 14:58:53 -0700

    created the 3-puts.c function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _puts - entry point
+ * Description:a function that prints a string
+ * followed by a new line, to stdout
+ * @str: string
+ * Return: void
+ */
+
+void _puts(char *str)
+
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
